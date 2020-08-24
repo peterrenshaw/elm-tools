43c89d402af62b484bd98a54942f411b466bdf2c
Peter Renshaw <peterrenshaw@seldomlogical.com>
2020-08-24 14:20:48 +1000

    2020AUG24 improving src/Main.elm compile, change time by including boiler plate for apps.

--- template/src/Main.elm
+++ template/src/Main.elm
@@ -9,13 +9,93 @@ module Main exposing (main)
    # sorc:
    #
 -}
+
+import Browser
+import Html
+
+
+
 -- CONST
 -- MAIN
+
+
+main =
+    Browser.Document
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+
 -- TYPES
+
+
+type Page
+    = InputPage
+
+
+type Msg
+    = NoOp
+
+
+type alias Flags =
+    ()
+
+
+
 -- MODEL
+
+
+type alias Model =
+    { currPage : Page
+    }
+
+
+
 -- FUNCS
 -- INIT
+
+
+init : Flags -> ( Model, Cmd Msg )
+init _ =
+    ( { currPage = InputPage
+      }
+    , Cmd.none
+    )
+
+
+
 -- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
 -- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        _ ->
+            ( model, Cmd.none )
+
+
+
 -- VIEW
+
+
+view : Model -> Browser.Document Msg
+view _ =
+    { title = "INSERT_TITLE_HERE"
+    , body = []
+    }
+
+
+
 {- eof -}
