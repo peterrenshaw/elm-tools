1f83a5d3365e2801348ee28c9377bdbd26c81bd9
Peter Renshaw <peterrenshaw@seldomlogical.com>
2020-08-27 14:30:19 +1000

    2020AUG27 added src/Blank.elm, README

---
+++ template/src/Blank.elm
@@ -0,0 +1,23 @@
+{- CHANGE MODULE NAME OR I WILL NOT COMPILE -}
+
+
+module Blank exposing (..)
+
+{-
+   #
+   # name: Blank.elm
+   # date:
+   # prog:
+   # desc: Lets create a new ...
+   #
+-}
+-- CONST
+-- MAIN
+-- TYPES
+-- MODEL
+-- UPDATE
+-- VIEW
+-- FUNCS
+-- INIT
+-- SUBSCRIPTIONS
+{- eof -}
