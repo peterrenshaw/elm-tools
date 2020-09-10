553383fca6c238ae7470a9b9fbb28f418e14597e
Peter Renshaw <peterrenshaw@seldomlogical.com>
2020-09-10 17:01:41 +1000

    2020SEP10 added src/*.elm. README

---
+++ template/src/Colour.elm
@@ -0,0 +1,72 @@
+module Colour exposing (..)
+
+{-
+   #
+   # name: Colour.elm
+   # date: 2020AUG13
+   # prog: pr
+   # desc: Colours used in  "slack ripp-off".
+   #
+-}
+
+import Element exposing (rgb255)
+
+
+
+-- CONST.colors
+
+
+colWhite =
+    rgb255 255 255 255
+
+
+colBlack =
+    rgb255 0 0 0
+
+
+colBlue =
+    rgb255 52 101 164
+
+
+colBlueLight =
+    rgb255 0xD8 0xE9 0xFF
+
+
+colPink =
+    rgb255 240 0 245
+
+
+colGrey =
+    rgb255 200 200 200
+
+
+colGreyLight =
+    rgb255 240 240 240
+
+
+colGreyDark =
+    rgb255 150 150 150
+
+
+colUnknown =
+    rgb255 117 179 201
+
+
+colGreen =
+    rgb255 0 150 50
+
+
+colGreenDark =
+    rgb255 0 100 0
+
+
+colCharcol =
+    rgb255 180 180 200
+
+
+colCharcolDark =
+    rgb255 100 100 150
+
+
+
+-- CONST.colors
---
+++ template/src/Emoji.elm
@@ -0,0 +1,135 @@
+module Emoji exposing (..)
+
+{-
+   #
+   # name: Emoji.elm
+   # date: 2020SEP10
+   #       2020AUG18
+   # prog: pr
+   # desc: Emoji for use
+   #
+-}
+
+
+msgPaper =
+    "📄"
+
+
+msgPencil =
+    "✏️"
+
+
+msgLabel =
+    "🏷️"
+
+
+msgTriangleUp =
+    "🔺"
+
+
+msgTriangleDown =
+    "🔻"
+
+
+msgHappy =
+    "\u{1F970}"
+
+
+msgSad =
+    "\u{1F97A}"
+
+
+msgSadVery =
+    "😢"
+
+
+msgMemo =
+    "📝"
+
+
+msgPostbox =
+    "📮"
+
+
+msgAddSomething =
+    "☝️"
+
+
+msgEnvelope =
+    "✉️"
+
+
+msgEnvelopeSend =
+    "📩"
+
+
+msgChick =
+    "🐥"
+
+
+msgRobot =
+    "\u{1F916}"
+
+
+msgDino =
+    "\u{1F996}"
+
+
+msgStego =
+    "\u{1F995}"
+
+
+msgTurtle =
+    "🐢"
+
+
+msgSnake =
+    "🐍"
+
+
+msgLizard =
+    "\u{1F98E}"
+
+
+msgSquid =
+    "🐙"
+
+
+msgDolphin =
+    "🐬"
+
+
+msgLobster =
+    "\u{1F99E}"
+
+
+msgCrab =
+    "\u{1F980}"
+
+
+msgWhaleOne =
+    "🐳"
+
+
+msgWhaleTwo =
+    "🐋"
+
+
+msgCamel =
+    "🐫"
+
+
+msgElephant =
+    "🐘"
+
+
+msgTurkey =
+    "\u{1F983}"
+
+
+msgBird =
+    "\u{1F99C}"
+
+
+msgFeet =
+    "🐾"
