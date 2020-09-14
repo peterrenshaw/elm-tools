f1ae0d40a0a8a7fa4a77cc77abc812ced99e225e
Peter Renshaw <peterrenshaw@seldomlogical.com>
2020-09-14 12:11:53 +1000

    2020SEP14 updated template/go.sh to allow for URL

--- template/go.sh
+++ template/go.sh
@@ -2,7 +2,8 @@
 
 # 
 # name: go.sh
-# date: 2020AUG21
+# date: 2020SEP14
+#       2020AUG21
 # prog: pr
 # desc: wrapper for HTTP server
 # args: 
@@ -13,24 +14,37 @@
 #        $ ./go.sh
 #        > starting server http://127.0.0.1:5000
 #
+#        $ ./go.sh 5000 192.168.0.60
+#        > option: port is 5000
+#        > option: URL  is 192.168.0.60
+#        > starting server http://192.168.0.60:5000
+#        
 { 
+
     # supply a PORT?
     if [ $1 ] ; then 
         PORT=$1
         echo "option: port is $PORT"
     else
         # no port, assume 5000
+        # ---- EDITABLE VALUE ----
         PORT=5000
+        # ---- EDITABLE VALUE ----
     fi 
 
-
-    # ---- EDIT THESE VALUES ----
-    URL="127.0.0.1"
-    # ---- EDIT THESE VALUES ----
-
+    # supply a URL?
+    if [ $2 ] ; then
+        URL="$2"
+        echo "option: url is <http://$URL>"
+    else
+        # no URL, assume localhost
+        # ---- EDITABLE VALUE ----
+        URL="127.0.0.1"
+        # ---- EDITABLE VALUE ----
+    fi
 
     echo "starting server $URL:$PORT"
-    python3 -m http.server --bind $URL $PORT
+    python3 -m http.server --bind $URL $PORT 
 
 } >&2
 
